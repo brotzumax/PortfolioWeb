16734cc5616a6af6a565039138c404ebad4cbd4c
Maximiliano <brotzumaxx@gmail.com>
2023-06-07 17:35:54 -0300

    Reset de estilos y nuevo scss del body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/CSS/main.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           ¡Hola Mundo!
         </p>
